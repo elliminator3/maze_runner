bf0bb05dc77c9df7c122d5e890fc12b168a9af81
Sarah Wellens <swellens@gmx.de>
2024-01-30 22:45:40 +0100

    final code + javadoc sarah // 30.01.

--- core/src/de/tum/cit/ase/maze/MenuScreen.java
+++ core/src/de/tum/cit/ase/maze/MenuScreen.java
@@ -2,7 +2,6 @@ package de.tum.cit.ase.maze;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Screen;
-import com.badlogic.gdx.graphics.Camera;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.scenes.scene2d.Actor;
@@ -13,7 +12,6 @@ import com.badlogic.gdx.scenes.scene2d.ui.TextButton;
 import com.badlogic.gdx.scenes.scene2d.utils.ChangeListener;
 import com.badlogic.gdx.utils.Scaling;
 import com.badlogic.gdx.utils.viewport.FillViewport;
-import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.badlogic.gdx.utils.viewport.Viewport;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
@@ -27,8 +25,10 @@ public class MenuScreen implements Screen {
 
     private final Stage stage;
     private Texture backgroundTexture;
+    private Image backgroundImage;
     private OrthographicCamera camera; // Moved camera to a field
     private Viewport viewport; // Moved viewport to a field
+
     /**
      * Constructor for MenuScreen. Sets up the camera, viewport, stage, and UI elements.
      *
@@ -37,19 +37,19 @@ public class MenuScreen implements Screen {
     public MenuScreen(MazeRunnerGame game) {
         camera = new OrthographicCamera(); // Initialization of camera with zoom and viewport with camera
         camera.setToOrtho(false, Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-        camera.zoom = 1.5f; // Set camera zoom for a closer view
+        camera.zoom = 1.1f; // Set camera zoom for a closer view
 
-        viewport = new FillViewport(Gdx.graphics.getWidth(), Gdx.graphics.getHeight(), camera); // Create a viewport with the camera
+        viewport = new FillViewport(Gdx.graphics.getWidth()*1.25f, Gdx.graphics.getHeight()*1.25f, camera); // Create a viewport with the camera
         stage = new Stage(viewport, game.getSpriteBatch()); // Create a stage for UI elements
+
         // Background image setup to fit the menu screen
         backgroundTexture = new Texture(Gdx.files.internal("Menu3.gif")); // Load the background image
-        Image backgroundImage = new Image(backgroundTexture); // Create an Image actor for the background
+        backgroundImage = new Image(backgroundTexture); // Create an Image actor for the background
         backgroundImage.setFillParent(true);
         backgroundImage.setBounds(0, 0, viewport.getWorldWidth(), viewport.getWorldHeight());
-        backgroundImage.setScaling(Scaling.stretch);
+        backgroundImage.setScaling(Scaling.fill);
 
         stage.addActor(backgroundImage);  // Add the background image to the stage
-
 
         Table table = new Table(); // Create a table for layout
         table.setFillParent(true); // Make the table fill the stage
@@ -113,6 +113,7 @@ public class MenuScreen implements Screen {
     @Override
     public void resize(int width, int height) {
         viewport.update(width, height, true);
+        backgroundImage.setSize(width,height);
         camera.position.set(camera.viewportWidth / 2, camera.viewportHeight / 2, 0);
     }
 
